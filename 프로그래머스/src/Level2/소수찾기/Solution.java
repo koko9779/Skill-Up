d4cd975f90753d3fe58bf34ae7231ddfce880c89
eunjung kim <ujandgd@naver.com>
2020-08-30 20:02:34 +0900

    소수찾기 어렵다잉..

--- 프로그래머스/src/Level2/소수찾기/Solution.java
+++ 프로그래머스/src/Level2/소수찾기/Solution.java
@@ -1,75 +1,89 @@
 package Level2.소수찾기;
 
-import java.util.HashSet;
+import java.util.Arrays;
 
-/*
- * numbers를 파싱해서 만들 수 있는 모든 수를 만든 후, 소수인지 확인
- * 
- * 1. 모든 순열을 구해서 확인
- * 2. 소수는 set에 저장 (중복방지)
- */
 public class Solution {
+	public static void main(String[] args) {
+		System.out.println(new Solution().solution("011"));
+	}
+	public int solution(String numbers) {
+		// 1. 주어진 숫자 조각들에서 구할 수 있는 가장 큰 값을 int형으로 반환한다
+		int maxNum = makeMaxNum(numbers.toCharArray());
+		
+		// 2. 가장 큰 값까지의 소수를 구한다
+		boolean[] primeNum = findPrimeNum(maxNum);
+		
+		// 3. 주어진 숫자조각으로 만들 수 있는 소수들의 개수를 구하여 반환한다
+		return findMakePossiblePrimeNum(primeNum, maxNum);
+	}
 	
-	static boolean[] decimal;		//소수
-	static int ans;
-	static char[] num;
-	static char[] select;			//순열
-	static HashSet<Integer> set;	//HashSet을 이용 -> 중복 제거, 소수라면 set에 저장
+	/***********************************************************************/
 	
-    public static int solution(String numbers) {
-    	
-    	num = numbers.toCharArray();	//numbers의 각 숫자값들을 구한다.
-    	
-    	decimal = new boolean[numbers.length()];
-    	ans = 0;
-    	set = new HashSet<>();
-    	 
-    	//numbers 모든 경우의 순열 구하기
-    	for(int i=1; i <= numbers.length(); i++) {
-    		select = new char[i];
-    		perm(0, i, numbers.length());
-    	}
-    	
-        return set.size();
-    }
-    
-    public static void perm(int start, int r, int n) {
-    	if(start == r) {
-    		//숫자로 변환
-    		String str = "";
-    		for(int i=0; i<select.length;i++)  str += select[i];   	
-    		int num = Integer.parseInt(str);
-    		
-    		//예외 처리
-    		if(num == 1 || num == 0) return;
-    		
-    		//소수인지 검사
-    		boolean flag = false;	//소수이면 false;
-    		for(int i=2; i<=Math.sqrt(num); i++) {
-    			if(num % i ==0) flag = true;	
-    		}
-    		
-    		//소수가 아니면
-    		if(!flag) set.add(num);
-    		
-    		return;
-    	}
-    	for (int i = 0; i < n; i++) {    		
-			if(!decimal[i]) {
-				decimal[i] = true;
-				select[start] = num[i];
-				perm(start+1, r, n);
-				decimal[i] = false;
+	// 1
+	private int makeMaxNum(char[] inputNum) {
+		Arrays.sort(inputNum);
+		int len = inputNum.length;
+		
+		//내림차순으로 변경
+		for (int i = 0; i < len/2; i++) {
+			char temp = inputNum[i];
+			inputNum[i] = inputNum[len-1-i];
+			inputNum[len-1-i] = temp;
+		}
+		return Integer.parseInt(new String(inputNum));
+	}
+	
+	// 2 에라토스테네스의 체
+	private boolean[] findPrimeNum(int maxNum) {
+		boolean[] result = new boolean[maxNum+1];	// default : false;
+		
+		for(int i = 2; i < Math.sqrt(maxNum); i++) {
+			if(!result[i]) {
+				// 소수가 아닌 값 : true;
+				for(int j = i * 2; j <= maxNum; j += i) {
+					result[j] = true;
+				}
 			}
 		}
-    }
-    
-    public static void main(String[] args) {
-    	String numbers = "17";
-    	String numbers2 = "011";
-    	
-    	System.out.println(solution(numbers));
-    	System.out.println(solution(numbers2));
-    }
-    
+		//소수=false 배열 return;
+		return result;
+	}
+	
+	// 3 
+	private int findMakePossiblePrimeNum(boolean[] primeNum, int maxNum) {
+		int possiblePrimeNumCount = 0;
+		
+		for(int primeNumIndex = 2; primeNumIndex <= maxNum; primeNumIndex++) {
+			//소수인지 && 주어진 숫자 조합으로 만들 수 있는 숫자인지
+			if(!primeNum[primeNumIndex] && isPossible(maxNum, primeNumIndex)) {
+				possiblePrimeNumCount ++;
+				//System.out.println(primeNumIndex);
+			}
+		}
+		
+		return possiblePrimeNumCount;
+	}
+	
+	//해당 소수가 주어진 숫자 조각으로 만들어질 수 있는지 여부를 판단
+	private boolean isPossible(int maxNum, int primeNum) {
+		int[] numCount = countAvailableNums(maxNum);
+		
+		while(primeNum != 0) {
+			if(numCount[primeNum % 10] <= 0) return false;
+			numCount[primeNum % 10] --;
+			primeNum /= 10;
+		}
+		
+		return true;
+	}
+	
+	//주어진 숫자조각에서 사용할 수 있는 0~9의 개수를 세기
+	private int[] countAvailableNums(int maxNum) {
+		char[] str = String.valueOf(maxNum).toCharArray();
+		int[] numCount = new int[10];
+		for(int i = 0; i < str.length; i++) {
+			numCount[str[i] - '0'] ++;
+		}
+		return numCount;
+	}
 }
