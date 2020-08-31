8594a61df4c08cd9b48163d44631c5b93d263bc7
eunjung kim <ujandgd@naver.com>
2020-08-31 16:33:54 +0900

    순열 알고리즘

---
+++ 백준/src/백준/완전탐색알고리즘/Main10974.java
@@ -0,0 +1,58 @@
+package 백준.완전탐색알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 문제
+ * N이 주어졌을 때, 1부터 N까지의 수로 이루어진 순열을 사전순으로 출력
+ * 
+ * 알고리즘
+ * 순열 -> n중 for문을 짤 수 없으니, 재귀 사용
+ */
+public class Main10974 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int N = Integer.parseInt(s);
+		
+		List number = new ArrayList<Integer>();
+		boolean[] check = new boolean[N+1]; 
+		
+		pick(N, number, check);
+
+	}
+
+	private static void pick(int n, List number, boolean[] check) {
+		//1~N까지 모두 들어왔을 때
+		if(number.size() == n) {
+			printAnswer(number);
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			if(check[i]) continue;
+			
+			//false -> true
+			check[i] = true;
+			number.add(i);
+			//check = 모두 true, number = 1,2,3...n
+			pick(n,number,check);	//number.size = full 일때 다음으로 넘어감
+			
+			number.remove(number.size()-1);	//맨뒤의 값 제거
+			check[i] = false;
+		}
+		
+	}
+
+	private static void printAnswer(List number) {
+		for (int i = 0; i < number.size(); i++) {
+			System.out.print(number.get(i)+" ");
+		}
+		System.out.println();
+	}
+	
+}
---
+++ 프로그래머스/src/Etc/Permutation.java
@@ -0,0 +1,44 @@
+package Etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 순열(Permutation) 
+ * : n개의 원소 중 r개의 원소를 꺼내는 경우의 수
+ * 표기법 : nPr = n!/(n-r)!
+ * 순서 유효(원소의 중복 허용, 조합은 순서가 유효하지 않아 중복 불허)
+ */
+public class Permutation {
+	public static void main(String[] args) {
+		List<String> arr = new ArrayList<>();
+		arr.add("a");
+		arr.add("b");
+		arr.add("c");
+		
+		List<String> result = new ArrayList<>();
+		reculsion(arr, result, arr.size(), 2);
+	}
+	/**
+	 * 순열 구하기
+	 * 
+	 * @param arr		: 기준 리스트
+	 * @param result	: 결과를 담아줄 리스트
+	 * @param n			: 전체 갯수
+	 * @param r			: 뽑을 갯수
+	 */
+	private static void reculsion(List<String> arr, List<String> result, int n, int r) {
+		// 뽑을 게 없으면 return;
+		if(r==0) {
+			System.out.println(result.toString());
+			return;
+		}
+		// 뽑을 게 있을 때, 총개수 중에 r개 뽑기
+		for (int i = 0; i < n; i++) {
+			result.add(arr.remove(i));
+			reculsion(arr, result, n-1, r-1);				// i를 제외한 나머지 중에서 r-1개의 경우의 수 (재귀)
+			arr.add(i, result.remove(result.size()-1));		// i: 원래있던 위치에 돌려놓기
+		}
+		
+	}
+}
