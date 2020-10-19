4aaab1b98b6a878ae293be8f54b8c47633a510a2
eunjung kim <ujandgd@naver.com>
2020-10-19 16:39:38 +0900

    쿼드트리 다시 보자

---
+++ Beakjoon/src/쿼드트리/Main1992.java
@@ -0,0 +1,20 @@
+package 쿼드트리;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+// 백준/별찍기/Main2447 관련 문제
+
+/*
+ * 1. 현재 구간에 있는 숫자들이 0으로만 이루어지거나 1로만 이루어져있다면 해당 숫자를 출력합니다.
+ * 2. 1번 조건이 성립하지 않을 경우 '('를 출력하고 2사분면, 1사분면, 3사분면, 4사분면 순서로 구간을 절반으로 잘라 재귀호출하고 ')'를 출력합니다.
+ * 3. 기저 사례는 구간의 크기가 1일 경우이고 구간의 크기가 1이면 해당 숫자를 출력합니다.
+ */
+public class Main1992 {
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+	}
+}
