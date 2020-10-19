9f3f92b2e10650d4c56addb29835074f1a728d43
eunjung kim <ujandgd@naver.com>
2020-10-19 16:45:00 +0900

    .

--- Beakjoon/src/쿼드트리/Main1992.java
+++ Beakjoon/src/쿼드트리/Main1992.java
@@ -3,7 +3,10 @@ package 쿼드트리;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 // 백준/별찍기/Main2447 관련 문제
+import java.util.StringTokenizer;
 
 /*
  * 1. 현재 구간에 있는 숫자들이 0으로만 이루어지거나 1로만 이루어져있다면 해당 숫자를 출력합니다.
@@ -11,10 +14,18 @@ import java.io.InputStreamReader;
  * 3. 기저 사례는 구간의 크기가 1일 경우이고 구간의 크기가 1이면 해당 숫자를 출력합니다.
  */
 public class Main1992 {
-	
-	
+	static String answer = "";
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		int [][] w = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				w[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		List<Integer> picked = new ArrayList<>();
+		boolean[] check = new boolean[n];
 	}
 }
