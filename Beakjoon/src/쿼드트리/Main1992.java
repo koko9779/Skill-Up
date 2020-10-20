394f5e7384495a41e90043ef7b296747b07cccbc
eunjung kim <ujandgd@naver.com>
2020-10-20 16:50:29 +0900

    쿼드트리 어렵군..

--- Beakjoon/src/쿼드트리/Main1992.java
+++ Beakjoon/src/쿼드트리/Main1992.java
@@ -14,18 +14,51 @@ import java.util.StringTokenizer;
  * 3. 기저 사례는 구간의 크기가 1일 경우이고 구간의 크기가 1이면 해당 숫자를 출력합니다.
  */
 public class Main1992 {
+	
 	static String answer = "";
-	public static void main(String[] args) throws IOException{
+	static int num = 0;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int [][] w = new int[n][n];
+		
+		// N*N 배열 만들기
+		int [][] arr = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				w[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		List<Integer> picked = new ArrayList<>();
-		boolean[] check = new boolean[n];
+		
+		quadTree(arr,0,0,arr.length);
+	}
+	
+	public static void quadTree(int[][] arr, int row, int col, int N) {
+		if(isAble(arr, row, col, N)) {
+			answer += num + ")";
+		}else {
+			
+			int size = N/2;
+			answer +="(";
+			
+			//해당 영역을 다시 4분할하여 탐색
+			quadTree(arr, row, col, size);
+            quadTree(arr, row, col + size, size);
+            quadTree(arr, row + size, col, size);
+            quadTree(arr, row + size, col + size, size);
+		}
+	}
+	
+	//해당 영역이 전부 1이거나 0인 경우 배열의 원소값 +1
+	public static boolean isAble(int[][] arr, int row, int col, int size) {
+		int t = arr[row][col];
+		num = arr[0][0];
+        for(int i=row; i < row+size; i++) {
+            for(int j=col; j < col+size; j++) {
+                if(t != arr[i][j]) return false;
+            }
+        }
+        return true;
 	}
 }
