a3eb23d9a658a2abdb7722324218a408e45a5bde
김은정 <ujandgd@naver.com>
2023-06-30 16:36:19 +0900

    vue.js

---
+++ Vue/intro-prj/src/views/ProvideInject.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>{{ message }}</div>
+</template>
+<script>
+import { computed } from 'vue'
+
+export default {
+  data() {
+    return {
+      message: '안녕!'
+    }
+  },
+  provide() {
+    return {
+      // 계산된 속성을 명시적으로 제공
+      message: computed(() => this.message)
+    }
+  }
+}
+</script>
